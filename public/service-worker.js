e9cc0377b9b471ff8fb1b585fef6b92dae1b2573
Nathan Knowler <nathan@knowler.dev>
2025-10-16 11:33:04 -0500

    Log the interception

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,6 @@
 self.addEventListener("fetch", event => {
 	if (event.request.method !== "POST") {
+		console.info("Skipping post request", event.request);
 		event.respondWith(fetch(event.request));
 		return;
 	}
@@ -8,6 +9,7 @@ self.addEventListener("fetch", event => {
 });
 
 async function handleShare(event) {
+	console.info("Handling share");
 	const formData = await event.request.formData();
 	const link = formData.get("link") || "";
 
